07aa252306f2f81d0891213a93294fb900cc1513
eleanew83 <eleanew83@berkeley.edu>
2026-02-25 04:12:08 +0000

    test2

--- rsync_retry.sh
+++ rsync_retry.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Edit these as needed before running..
+# Edit these as needed before running.
 SOURCE="/Users/eleanew83/Documents/OneDrive - University of Cambridge/Cambridge/IG_Photos/"
 DEST="ylj20@login.hpc.cam.ac.uk:/home/ylj20/rds/hpc-work/IG_Photos/"
 # SOURCE="/Users/eleanew83/Documents/OneDrive - University of Cambridge/Cambridge/FacialRecognitionTest/yolo_detection/yolo_detection_data.tar.gz"
